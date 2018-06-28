6db8daaea825cb11033dada8f68624718d626007
Matteo Melli <matteom@8kdata.com>
2018-06-28 15:39:51 +0200

    Some fixes

--- start-docker-with-ansible.sh
+++ start-docker-with-ansible.sh
@@ -6,7 +6,7 @@ ANSIBLE_PLAYBOOK="${1}"
 ANSIBLE_HOST="${2:-localhost}"
 ANSIBLE_USER="${3:-$USER}"
 ANSIBLE_KEY="${4:-$HOME/.ssh/id_rsa}"
-
+shift 4
 
 if [ -z "$ANSIBLE_PLAYBOOK" ]
 then
@@ -24,6 +24,7 @@ docker run \
   teamidefix/ansible bash
 else
 docker run \
+  -ti \
   --rm \
   -w /ansible-oracle/ \
   -e ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK} \
@@ -33,5 +34,6 @@ docker run \
   -v $(realpath "$(dirname "$0")"):/ansible-oracle \
   -v $HOME:$HOME:ro -v /etc/passwd:/etc/passwd:ro \
   -u $(id -u) \
+  "$@" \
   teamidefix/ansible bash run-playbook-inside-docker.sh "$ANSIBLE_PLAYBOOK"
 fi
